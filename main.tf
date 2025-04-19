9739d2488e2638b20f98b563efda8858a67d6a39
Randy Pope <rapope@cisco.com>
2025-04-19 10:50:45 -0400

    added private route tables

--- main.tf
+++ main.tf
@@ -3,53 +3,39 @@
 #################################################################################################################################
 
 resource "aws_vpc" "app_vpc" {
-#  count                = 1
   cidr_block           = "${var.ip_prefix}.0/24"
   enable_dns_support   = true
   enable_dns_hostnames = true
   instance_tenancy     = "default"
   tags = {
     Name       = "vpc-${var.project_name}"
-#    owner      = var.owner
-#    pod        = "pod${var.pod_number}"
   }
 }
 
 resource "aws_subnet" "public_subnet" {
-#  count             = 1
   vpc_id            = aws_vpc.app_vpc.id
   cidr_block        = "${var.ip_prefix}.0/26"
   availability_zone = var.availability_zone 
   tags = {
     Name       = "sn-${var.project_name}-public"
-#    owner      = var.owner
-#    pod        = "pod${var.pod_number}"
   }
 }
 
 resource "aws_subnet" "private_subnet" {
-#  count              = 1
   vpc_id            = aws_vpc.app_vpc.id
   cidr_block        = "${var.ip_prefix}.64/26"
   availability_zone = var.availability_zone
-#  availability_zone = "us-east-1a"
   tags = {
     Name       = "sn-${var.project_name}-private"
-#    owner      = var.owner
-#    pod        = "pod${var.pod_number}"
   }
 }
 
 resource "aws_subnet" "tgw_subnet" {
-#  count              = 1
   vpc_id            = aws_vpc.app_vpc.id
   cidr_block        = "${var.ip_prefix}.192/26"
   availability_zone = var.availability_zone
-#  availability_zone = "us-east-1a"
   tags = {
     Name       = "sn-${var.project_name}-tgw"
-#    owner      = var.owner
-#    pod        = "pod${var.pod_number}"
   }
 }
 
@@ -64,7 +50,33 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment" {
   vpc_id             = aws_vpc.app_vpc.id   
   tags = {
     Name       = "tgw-attachment-${var.project_name}"
-#    owner      = var.owner
-#    pod        = "pod${var.pod_number}"
   }
+}
+##################################################################################################################################
+# Routing Tables and Routes
+##################################################################################################################################
+
+resource "aws_route_table" "private_routes" {
+  vpc_id = aws_vpc.app_vpc.id
+  tags = {
+    Name       = "rt-${var.project_name}-private"
+  }
+}
+
+resource "aws_route" "private_10_route" {
+  route_table_id         = aws_route_table.private_routes.id
+  destination_cidr_block = "10.0.0.0/8"
+  gateway_id             = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment.id
+}
+
+resource "aws_route" "private_172_route" {
+  route_table_id         = aws_route_table.private_routes.id
+  destination_cidr_block = "172.16.0.0/12"
+  gateway_id             = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment.id
+}
+
+resource "aws_route" "private_192_route" {
+  route_table_id         = aws_route_table.private_routes.id
+  destination_cidr_block = "192.168.0.0/16"
+  gateway_id             = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment.id
 }
